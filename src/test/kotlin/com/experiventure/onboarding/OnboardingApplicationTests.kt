2f0f09b23ebaf2ab5b8e5e0801e2c0e8f98c656e
noah.you <noah.you@kakaopaycorp.com>
2021-09-14 22:03:18 +0900

    first commit

---
+++ src/test/kotlin/com/experiventure/onboarding/OnboardingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.experiventure.onboarding
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OnboardingApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
